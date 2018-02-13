e7f6d0eda3b990e43a3ace1011554ba3834ac641
root <steven.briscoe@intel.com>
2018-02-13 15:41:20 +0000

    Fix hbase docker image

--- docker-hbase/scripts/entrypoint.sh
+++ docker-hbase/scripts/entrypoint.sh
@@ -1,5 +1,7 @@
+#!/bin/bash
+
 #
-# Copyright (c) 2017 Intel Corporation
+# Copyright (c) 2018 Intel Corporation
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -11,9 +13,7 @@
 # distributed under the License is distributed on an "AS IS" BASIS,
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
-# limitations under the License.
-
-#!/bin/bash
+# limitations under the License
 
 /opt/replace-hostname.sh
 
