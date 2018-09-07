569d32f2b0c83cd38c984382ca4dccb75ea021ab
Ali Rasim Kocal <arkocal@gmail.com>
2018-09-07 06:57:55 +0200

    Major changes to docker-compose and makefile
    * Removed docker-compose-quick, a single file is used for pulling and manually building.
    * Refactored Makefile heavily, and added documentation
    * Moved to a new version of docker-compose in platform setup
    * Made the matching changes to setup-environment.example.sh

--- setup-environment.example.sh
+++ setup-environment.example.sh
@@ -14,6 +14,8 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+export DOCKER_TAG=${DOCKER_TAG:-'latest'}
+
 export ZOOKEEPER_KAFKA='kafka'
 export ZOOKEEPER_KAFKA_PORT='2181'
 export ZOOKEEPER_HBASE='hbase'
