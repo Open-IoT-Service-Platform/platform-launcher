7ada8acebcb31b3c49dfd5ccf32ef8af78917c7d
Ali Rasim Kocal <arkocal@gmail.com>
2019-01-08 14:39:26 +0100

    docker-compose.yml: Move Gearpump to new config system
    Signed-off-by: Ali Rasim Kocal <arkocal@gmail.com>

--- setup-environment.example.sh
+++ setup-environment.example.sh
@@ -37,6 +37,7 @@ export KAFKA_PORT='9092'
 export KAFKA_HEARTBEAT_TOPIC='heartbeat'
 export GEARPUMP='gearpump:8090'
 export BACKEND='backend:8080'
+export FRONTEND='frontend:4001'
 export NGINX='nginx'
 export REDIS='redis'
 export REDIS_PORT='6379'
@@ -57,183 +58,6 @@ OPENTSDB_PROPERTIES='{
   "port": "'$OPENTSDB_PORT'"
 }'
 
-
-#################### VCAP is deprecated, will be removed in next version
-export VCAP_SERVICES='{
-"postgresql93": [{
-    "credentials": {
-    "dbname": "'$POSTGRES_DB'",
-    "hostname": "'$POSTGRES'",
-    "password": "'$POSTGRES_PASSWORD'",
-    "port": "'$POSTGRES_PORT'",
-    "username": "'$POSTGRES_USERNAME'"
-    },
-    "name": "mypostgres"
-}],
-    "websocket-server": [{
-        "server-name": "websocket-server",
-        "name": "mywsserver"
-}],
-"hbase": [{
-    "credentials": {
-    "HADOOP_CONFIG_KEY": {
-        "hadoop.security.authentication": "simple",
-        "hadoop.security.authorization": "false",
-        "hbase.security.authentication": "simple",
-        "ha.zookeeper.quorum": "'$ZOOKEEPER_HBASE'",
-        "hbase.zookeeper.property.clientPort": "'$ZOOKEEPER_HBASE_PORT'",
-        "hbase.zookeeper.quorum": "'$ZOOKEEPER_HBASE'"
-    }
-    },
-    "name": "myhbase"
-}],
-"hdfs": [{
-    "credentials": {
-    "HADOOP_CONFIG_KEY": {
-        "ha.zookeeper.quorum": "'${ZOOKEEPER_HBASE}':'${ZOOKEEPER_HBASE_PORT}'",
-        "hadoop.security.authorization": "false"
-    }
-    },
-    "name": "myhdfs"
-}],
-"kafka": [{
-    "credentials": {
-    "uri": "'$KAFKA'"
-    },
-    "name": "mykafka"
-}],
-"zookeeper": [{
-    "credentials": {
-     "zk.cluster": "'${ZOOKEEPER_KAFKA}:${ZOOKEEPER_KAFKA_PORT}'",
-     "zk.node": "/tmp"
-    },
-    "label": "zookeeper",
-    "name": "myzookeeper",
-    "plan": "local",
-    "tags": []
-}],
-"gearpump": [{
-    "credentials": {
-     "username": "admin",
-     "password": "admin",
-     "dashboardUrl": "'$GEARPUMP'"
-    },
-    "name": "mygearpump"
-}],
-"smtp": [
-   {
-    "credentials": {
-     "host": "'$SMTP_HOST'",
-     "port": "'$SMTP_PORT'",
-     "protocol": "smtp",
-     "username": "'$SMTP_USERNAME'",
-     "password": "'$SMTP_PASSWORD'"
-    },
-    "label": "smtp",
-    "name": "mysmtp"
-   }
-  ],
-"redis": [
-   {
-    "credentials": {
-     "hostname": "'$REDIS'",
-     "port": "'$REDIS_PORT'",
-     "password": ""
-    },
-    "label": "redis",
-    "name": "myredis"
-   }
-  ],
-"user-provided": [{
-    "credentials": {
-    "kdc": "localhost",
-    "kpassword": "pass",
-    "krealm": "realm",
-    "kuser": "user"
-    },
-    "name": "kerberos-service"
-},
-{
-    "credentials": {
-    "hosts": "'$KAFKA'",
-    "enabled": true,
-    "partitions": 1,
-    "replication": 1,
-    "timeout_ms": 10000,
-    "topics": {
-                    "observations": "metrics",
-                    "rule_engine": "rules-update",
-                    "heartbeat": {
-                        "name": "'$KAFKA_HEARTBEAT_TOPIC'",
-                        "interval": 5000
-                    }
-                }
-    },
-    "name": "kafka-ups"
-},
-{
-    "credentials": {
-    "host": "http://'$NGINX'",
-    "strictSSL": false
-    },
-    "name": "dashboard-endpoint-ups"
-},
-{
-    "credentials": {
-    "captcha_test_code": "s09ef48213s8fe8fw8rwer5489wr8wd5",
-    "interaction_token_permision_key": "password",
-    "private_pem_path": "./keys/private.pem",
-    "public_pem_path": "./keys/public.pem"
-    },
-    "name": "dashboard-security-ups"
-},
-{
-    "credentials": {
-    "deviceMeasurementTableName": "LOCAL-AA-BACKEND_DEVICE_MEASUREMENT",
-    "host": "http://'$BACKEND'"
-
-    },
-    "name": "backend-ups"
-},
-{
-    "credentials": {
-    "sender": "test.sender@streammyiot.com"
-    },
-    "name": "mail-ups"
-},
-{
-    "credentials": {
-    "password": "7d501829lhbl1or0bb1784462c97bcad6",
-    "username": "gateway@intel.com"
-
-    },
-    "name": "gateway-credentials-ups"
-},
-{
-    "credentials": {
-    "password": "7d501829lhbl1or0bb1784462c97bcad6",
-    "username": "rule_engine@intel.com"
-
-    },
-    "name": "rule-engine-credentials-ups"
-},
-{
-    "credentials": {
-    "password": "AgjY7H3eXztyA6AmNjI2rte446gdttgattwRRF61",
-    "username": "api_actuator"
-    },
-    "name": "websocket-ups"
-}]
-}'
-
-export VCAP_APPLICATION='{
-    "space_name": "local"
-}'
-#########################################
-
-#VCAP variables are left overs from Cloud Foundry times.
-#To allow a transition to Kubernetes, we will define both, VCAP and regular environment variables.
-#The following is how the configuration looks in future
 #The root variable for every service looks like OISP_SERVICE_CONFIG
 #It contains a JSON object, starting with single quote, hash names with double quotes, environmental variables with double, then single quotes, e.g. "'$VAR'"
 #References to other environmental variables which can be parsed as OBJECTS are done with "@@OISP_*_CONFIG"
@@ -376,8 +200,15 @@ export OISP_MAIL_CONFIG=\
 
 export OISP_RULEENGINE_CONFIG=\
 '{
+    "uri": "gearpump:8090",
     "password": "7d501829lhbl1or0bb1784462c97bcad6",
-    "username": "rule_engine@intel.com"
+    "username": "rule_engine@intel.com",
+    "gearpumpUsername": "admin",
+    "gearpumpPassword": "admin",
+    "frontendUri": "'$FRONTEND'",
+    "hbaseConfig": "@@OISP_HBASE_CONFIG",
+    "kafkaConfig": "@@OISP_KAFKA_CONFIG",
+    "zookeeperConfig": "@@OISP_ZOOKEEPER_CONFIG"
 }'
 
 export OISP_GATEWAY_CONFIG=\
