52e4d51895d1a1b507c9522866e1209a9a7d7e35
Ali Rasim Kocal <arkocal@gmail.com>
2019-08-01 16:30:23 +0200

    K8S: Fixes #25 , wait for tiller to deploy
    Signed-off-by: Ali Rasim Kocal <arkocal@gmail.com>

--- util/setup-ubuntu18.04.sh
+++ util/setup-ubuntu18.04.sh
@@ -59,7 +59,7 @@ sudo snap install helm --classic
 
 kubectl -n kube-system create serviceaccount tiller
 kubectl create clusterrolebinding tiller --clusterrole=cluster-admin --serviceaccount=kube-system:tiller
-helm init --service-account tiller
+helm init --service-account tiller --wait
 echo "\033[1mHelm initiated succesfully.\033[0m"
 
 echo ""
