8ecfe35f50a96fe0bc2db168c9ddcdb2c360495d
Ali Rasim Kocal <arkocal@gmail.com>
2019-09-04 10:39:10 +0200

    .circleci: Moved tests to kubernetes
    Signed-off-by: Ali Rasim Kocal <arkocal@posteo.net>

--- kubernetes/util/build-and-import-images.sh
+++ kubernetes/util/build-and-import-images.sh
@@ -1,9 +1,7 @@
 #! /bin/bash
 
 BRANCH=${BRANCH:-develop}
-K3S_NODE=$(docker ps --format '{{.Names}}' | grep k3s_node)
 export TEST=1
-export DOCKER_TAG=test
 
 cd /tmp
 rm -rf platform-launcher/
@@ -12,11 +10,4 @@ cd platform-launcher
 git submodule update
 make update
 printf "n\ny\ny\n" | make build DEBUG=true
-
-for image in debugger hbase mqtt-gateway websocket-server frontend backend rule-engine opentsdb mqtt-broker grafana
-do
-    printf $image && \
-    docker save oisp/$image:$DOCKER_TAG -o $image && printf " is saved" && \
-    docker cp $image $K3S_NODE:/tmp/$image && printf ", copied" && \
-    docker exec -it $K3S_NODE ctr image import /tmp/$image >> /dev/null && printf ", imported\n"
-done;
+/bin/bash import-images.sh
