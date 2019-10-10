b631eac60e40c634018bec8faebf77e300d55019
Oguzcan Kirmemis <oguzcan.kirmemis@gmail.com>
2019-10-10 12:42:31 +0200

    Added test: Create and Activate a device with an existing id in a different account
    Signed-off-by: Oguzcan Kirmemis <oguzcan.kirmemis@gmail.com>

--- tests/subtests/device-activation-tests.js
+++ tests/subtests/device-activation-tests.js
@@ -1,23 +1,22 @@
-var test = function(userToken, accountId) {
+var test = function(userToken, accountId1) {
     var promtests = require('./promise-wrap');
-    var activationCode;
+    var activationCode1;
+    var activationCode2;
+    var accountId2;
     var deviceId1 = "ACTIVATION-TEST1";
     var deviceId2 = "ACTIVATION-TEST2";
-    var deviceToken1;
-    var deviceToken2;
 
     return {
         "prepareSetup": function(done) {
-            promtests.createDevice("device1", deviceId1, userToken, accountId)
-            .then(() => promtests.getAccountActivationCode(accountId, userToken))
-            .then((res) => { activationCode = res.activationCode; })
+            promtests.createDevice("device1", deviceId1, userToken, accountId1)
+            .then(() => promtests.getAccountActivationCode(accountId1, userToken))
+            .then((res) => { activationCode1 = res.activationCode; })
             .then(() => { done(); })
             .catch((err) => { done(err); });
         },
         "activateExistingDeviceWithoutToken": function(done) {
-            promtests.activateDeviceWithoutToken(activationCode, deviceId1).then((res) => {
+            promtests.activateDeviceWithoutToken(activationCode1, deviceId1).then((res) => {
                 if (res.deviceToken) {
-                    deviceToken1 = res.deviceToken;
                     done();
                 } else {
                     done('Cannot activate device without user token.');
@@ -26,10 +25,24 @@ var test = function(userToken, accountId) {
                 done(err);
             });
         },
-        "activateNotExistingDeviceWithoutToken": function(done) {
-            promtests.activateDeviceWithoutToken(activationCode, deviceId2).then((res) => {
+        "activateAnotherDeviceWithSameIdInAnotherAccount": function(done) {
+            promtests.createAccount("ExistingDeviceIdTest", userToken)
+            .then((res) => { accountId2 = res.id; })
+            .then(() => promtests.createDevice("device1", deviceId1, userToken, accountId2))
+            .then(() => promtests.getAccountActivationCode(accountId2, userToken))
+            .then((res) => { activationCode2 = res.activationCode; })
+            .then(() => promtests.activateDeviceWithoutToken(activationCode2, deviceId1)).then((res) => {
                 if (res.deviceToken) {
-                    deviceToken1 = res.deviceToken;
+                    done();
+                } else {
+                    done('Cannot activate device with same id in another account.');
+                }
+            })
+            .catch((err) => { done(err); });
+        },
+        "activateNotExistingDeviceWithoutToken": function(done) {
+            promtests.activateDeviceWithoutToken(activationCode1, deviceId2).then((res) => {
+                if (res.deviceToken) {
                     done();
                 } else {
                     done('Cannot activate not existing device without token');
@@ -39,8 +52,9 @@ var test = function(userToken, accountId) {
             });
         },
         "cleanup": function(done) {
-            promtests.deleteDevice(userToken, accountId, deviceId1)
-            .then(() => { promtests.deleteDevice(userToken, accountId, deviceId2) })
+            promtests.deleteDevice(userToken, accountId1, deviceId1)
+            .then(() => { promtests.deleteDevice(userToken, accountId1, deviceId2) })
+            .then(() => promtests.deleteAccount(userToken, accountId2))
             .then(() => { done(); })
             .catch((err) => { done(err); });
         }
@@ -50,6 +64,7 @@ var test = function(userToken, accountId) {
 var descriptions = {
     "prepareSetup": "Create device for subtest",
     "activateExistingDeviceWithoutToken": "Shall activate a device only with activation code",
+    "activateAnotherDeviceWithSameIdInAnotherAccount": "Shall create and activate another device with same id in different account",
     "activateNotExistingDeviceWithoutToken": "Shall create and activate device only with activation code",
     "cleanup": "Cleanup devices that are created for subtest",
 };
