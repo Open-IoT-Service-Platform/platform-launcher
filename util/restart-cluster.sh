9ad6f2a2f3d38d0d311e894bac689fbf1a53bc79
Marcel Wagner <wagmarcel@web.de>
2020-02-19 08:32:21 +0100

    Removed Kafka/Zookeeper operator. Now using helm charts. * Added kafka and kafka-test subcharts with different configs * Subchart selection: kafka or kafka-test selected based on values less_resources and production * Config updated to prepare kafka-ha patches in other projects * "Production" Kafka setup with 3 brokers, 2 x replicationFactor and 2 x Partitions for metrics
    Signed-off-by: Marcel Wagner <wagmarcel@web.de>

--- util/restart-cluster.sh
+++ util/restart-cluster.sh
@@ -1,21 +1,21 @@
-printf "\033[1mStarting k3s\n"
-printf -- "------------\033[0m\n"
-rm -rf ~/k3s
-mkdir ~/k3s
-curl https://raw.githubusercontent.com/rancher/k3s/release/v1.0/docker-compose.yml > ~/k3s/docker-compose.yml
+#printf "\033[1mStarting k3s\n"
+#printf -- "------------\033[0m\n"
+#rm -rf ~/k3s
+#mkdir ~/k3s
+#curl https://raw.githubusercontent.com/rancher/k3s/release/v1.0/docker-compose.yml > ~/k3s/docker-compose.yml
 # Compose down is necessary for subsequent runs to succeed
-cd ~/k3s && sudo docker-compose down -v && sudo docker-compose up -d
-printf "Waiting for k3s to create kubeconfig file\n"
-while [ ! -f ~/k3s/kubeconfig.yaml ]
-do
-  sleep 1
-done
-if [ -f ~/.kube/config ]; then
-   printf "Backing-up old config in ~/.kube/config_beforeoisp.bak\n"
-   mv ~/.kube/config ~/.kube/config_beforeoisp.bak
-fi
-cp ~/k3s/kubeconfig.yaml ~/.kube/config
-sleep 5
+#cd ~/k3s && sudo docker-compose down -v && sudo docker-compose up -d
+#printf "Waiting for k3s to create kubeconfig file\n"
+#while [ ! -f ~/k3s/kubeconfig.yaml ]
+#do
+#  sleep 1
+#done
+#if [ -f ~/.kube/config ]; then
+#   printf "Backing-up old config in ~/.kube/config_beforeoisp.bak\n"
+#   mv ~/.kube/config ~/.kube/config_beforeoisp.bak
+#fi
+#cp ~/k3s/kubeconfig.yaml ~/.kube/config
+#
 printf "\033[1mKubernetes cluster started\033[0m\n"
 kubectl cluster-info
 printf "\n"
