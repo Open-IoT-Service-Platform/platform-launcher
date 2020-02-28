54093e4d8c750dbaf707becee74a35d0e7d5db73
Oguzcan Kirmemis <oguzcan.kirmemis@gmail.com>
2020-02-28 15:09:28 +0300

    Keycloak: Added keycloak container
    - Adjusted Makefile and Values.yaml
    - Added OISP Event Listener For User Delete Events
    - Deployed JS Policies and Mappers as a JAR Module
    
    Signed-off-by: Oguzcan Kirmemis <oguzcan.kirmemis@gmail.com>

---
+++ docker/keycloak/prepare-startup.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+echo "Database address is set to: $DB_ADDR"
+echo "OISP Frontend User Account Endpoint is set to: $OISP_FRONTEND_USER_ACCOUNT_ENDPOINT"
+echo "OISP Frontend Device Account Endpoint is set to: $OISP_FRONTEND_DEVICE_ACCOUNT_ENDPOINT"
+echo "OISP Frontend User Delete Endpoint is set to: $OISP_FRONTEND_USER_DELETE_ENDPOINT"
+REALM_DIR="/opt/jboss/keycloak/realms/oisp-realm.json"
+PRIVATE_KEY=$(cat /keys/private.pem)
+PRIVATE_KEY=${PRIVATE_KEY//$'\n'/\\\\n}
+PRIVATE_KEY=${PRIVATE_KEY//\//\\\/}
+PUBLIC_CERT=$(cat /keys/x509.pem)
+PUBLIC_CERT=${PUBLIC_CERT//$'\n'/\\\\n}
+PUBLIC_CERT=${PUBLIC_CERT//\//\\\/}
+sed -i "s/{{ INSERT-PRIVATE-KEY }}/${PRIVATE_KEY}/" ${REALM_DIR}
+sed -i "s/{{ INSERT-PUBLIC-CERT }}/${PUBLIC_CERT}/" ${REALM_DIR}
+# SECRET variables are already defined in the environment variables
+sed -i "s/{{ INSERT-OISP-FRONTEND-SECRET }}/${OISP_FRONTEND_SECRET}/" ${REALM_DIR}
+sed -i "s/{{ INSERT-MQTT-BROKER-SECRET }}/${OISP_MQTT_BROKER_SECRET}/" ${REALM_DIR}
