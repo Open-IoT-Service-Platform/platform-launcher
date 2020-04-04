18a687d360828db744cbc2a4444e8071e6574e5b
Marcel Wagner <wagmarcel@web.de>
2020-04-05 00:10:03 +0200

    Added test for checking larger setups like 100s of devices or 20k samples Needed to avoid regression of bugs which only appear with larger quantity Test is disabled by default to make sure the low resource circleci tests are not failing
    Signed-off-by: Marcel Wagner <wagmarcel@web.de>

--- util/debug/backend/prepare_debug.sh
+++ util/debug/backend/prepare_debug.sh
@@ -22,6 +22,7 @@ source <(../../get_oisp_container_config.sh ${NAMESPACE} ${BACKEND})
 IPADDRESS=$(hostname -I | cut -d " " -f 1)
 echo selected IP address ${IPADDRESS}
 kubectl -n ${NAMESPACE} delete svc backend
+kubectl -n ${NAMESPACE} delete deployment backend
 cat << EOF | kubectl -n ${NAMESPACE} create -f -
 apiVersion: v1
 kind: Service
@@ -49,4 +50,10 @@ subsets:
     name: "8080"
 EOF
 echo all prepared
+echo recommended settings for kubeflow
+echo kubectl -n oisp label svc oisp-kafka-headless app=kafka
+echo sudo -E KUBECONFIG=~/k3s/kubeconfig.yaml kubefwd -n oisp svc -l "app in (kairosdb, kafka, minio)"
+echo \#sudo ip route add 10.42.0.0/16 via \<ip of k3s-agent\> \# needed to use kafka, since kafka uses ip addresses rather domain names. kubefwd does not route ip addresses.
+echo Make sure that backend is listening to port 8081\(\!\) not to 8080. Reason kubeforward is using localhost and thus cannot forward two 8080 ports \(and kairosdb uses 8080 as well\)
+echo configure the backend:8081 in cm/oisp-config
 popd
