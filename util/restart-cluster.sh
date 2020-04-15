b014cbdb3701d0aba75d9a5e60ecc5ee76b6def2
Marcel Wagner <wagmarcel@web.de>
2020-04-15 18:10:34 +0200

    Cert-Manager integration with MQTT * Added default self-signed certificate from cert-manager * Added self-signed Cluster-Issuer in restart-cluster (allows easy replacement by prod issuer from letsencrypt) * Updated MQTT stateful-sets to reference the secreate created by cert-manager * Added cert-manager section to values.yaml
    Signed-off-by: Marcel Wagner <wagmarcel@web.de>

--- util/restart-cluster.sh
+++ util/restart-cluster.sh
@@ -24,6 +24,22 @@ printf "\033[1mGot storage classes:\033[0m\n"
 kubectl get storageclass
 printf "\n"
 
+printf "\033[1mSet routing between docker and host for K3S\033[0m\n"
+# Needed for Kafka testing and cert-Manager
+AGENTID=$(docker ps | grep agent| awk '{print $1}')
+AGENTIP=$(docker inspect -f '{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' ${AGENTID})
+sudo route add -net 10.42.0.0 netmask 255.255.0.0 gw ${AGENTIP}
+sudo route add -net 10.43.0.0 netmask 255.255.0.0 gw ${AGENTIP}
+
+printf "\033[1mWaiting for traefik to come up\033[0m\n"
+while [ -z "$(kubectl -n kube-system get pods -l=app=traefik --ignore-not-found)" ]; do
+ printf "."; sleep 5;
+done
+while kubectl -n kube-system get pods -l=app=traefik -o jsonpath="{.items[*].status.containerStatuses[*].ready}" | grep false >> /dev/null; do
+ printf "."; sleep 5;
+done;
+printf "\033[1m\nTraefik ready!\033[0m\n"
+
 popd
 ./deploy_operators.sh
 
