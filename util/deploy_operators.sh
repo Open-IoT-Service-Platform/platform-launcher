628e1ecfc02748f304bc80eb3c9dbd30ea9cb05c
Marcel Wagner <wagmarcel@web.de>
2020-04-15 18:10:34 +0200

    Cert-Manager integration with MQTT * Added default self-signed certificate from cert-manager * Added self-signed Cluster-Issuer in restart-cluster (allows easy replacement by prod issuer from letsencrypt) * Updated MQTT stateful-sets to reference the secreate created by cert-manager * Added cert-manager section to values.yaml
    Signed-off-by: Marcel Wagner <wagmarcel@web.de>

--- util/deploy_operators.sh
+++ util/deploy_operators.sh
@@ -14,4 +14,17 @@ kubectl -n cassandra apply -f https://raw.githubusercontent.com/instaclustr/cass
 kubectl -n cassandra apply -f https://raw.githubusercontent.com/instaclustr/cassandra-operator/v3.1.1/deploy/bundle.yaml
 kubectl -n cassandra delete cm cassandra-operator-default-config && \
 
+printf "\n"
+printf "\033[1mInstalling cert-manager\n"
+printf -- "------------------------\033[0m\n"
+kubectl apply --validate=false -f https://github.com/jetstack/cert-manager/releases/download/v0.14.1/cert-manager.yaml
+printf "\033[1mWaiting for cert-manager web-hook to come up\033[0m\n"
+while [ -z "$(kubectl -n cert-manager get pods -l=app=webhook --ignore-not-found)" ]; do
+  printf "."; sleep 5;
+done
+while kubectl -n cert-manager get pods -l=app=webhook -o jsonpath="{.items[*].status.containerStatuses[*].ready}" | grep false >> /dev/null; do
+  printf "."; sleep 5;
+done;
+printf "\033[1m\nCert-manager Webhook ready! Now applying clusterissuer for self-cert.\033[0m\n"
+kubectl apply -f ../kubernetes/cert-manager/clusterissuer-self-cert.yaml
 printf -- "\033[1mOperators installed successfully.\033[0m\n"
