d18bef35f7cb35dd63673108f1dea49e792a7e10
Ali Rasim Kocal <arkocal@posteo.net>
2020-07-07 14:14:02 +0200

    Integrating services operator and migrating rule engine.
    Signed-off-by: Ali Rasim Kocal <arkocal@posteo.net>

--- ci_deployer/deploy.sh
+++ ci_deployer/deploy.sh
@@ -3,11 +3,10 @@
 git clone https://github.com/Open-IoT-Service-Platform/platform-launcher.git -b develop
 cd platform-launcher
 echo y | make update
-helm init --upgrade --wait
 export DOCKERUSER=${DOCKER_USERNAME}
 export DOCKERPASS=${DOCKER_PASSWORD}
 export NODOCKERLOGIN=true
-if [[ $(helm ls -q | grep "${NAME}\$") ]]; then
+if [[ $(helm ls -q --all-namespaces | grep "${NAME}\$") ]]; then
     CMD="upgrade"
     rm kubernetes/templates/jobs/db_setup.yaml
 else
