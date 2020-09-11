08541ecdba2f717a811e5f51ce14b46c5ea51140
Marcel Wagner <wagmarcel@web.de>
2020-09-11 17:02:13 +0200

    Added values to make sure that certificate is not overwritten in auto-deployment Added --force flag to helm args to force replacement of configs.
    Signed-off-by: Marcel Wagner <wagmarcel@web.de>

--- ci_deployer/deploy.sh
+++ ci_deployer/deploy.sh
@@ -12,4 +12,9 @@ if [[ $(helm ls -q --all-namespaces | grep "${NAME}\$") ]]; then
 else
     CMD="deploy"
 fi;
+export HELM_ARGS="--force --set less_resources=\"false\" --set production=\"true\" \
+		--set certmanager.secret="frontend-web-prod-tls" \
+                --set certmanager.issuer="letsencrypt-prod" \
+                --set numberReplicas.frontend=2 \
+                --set numberReplicas.backend=3"
 make ${CMD}-oisp DOCKER_TAG=nightly-$(date +"%Y-%m-%d")
