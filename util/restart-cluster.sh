96c239ef006350d7b6bfdce03a16f64699bc891f
Ali Rasim Kocal <arkocal@posteo.net>
2021-04-11 14:49:06 +0200

    K3D-Migration: Import images
    Update import command in makefile to use local registry.

--- util/restart-cluster.sh
+++ util/restart-cluster.sh
@@ -4,7 +4,7 @@ SCRIPTDIR=$(dirname "$0")
 CLUSTERNAME=${CLUSTERNAME:-oispcluster}
 
 k3d cluster delete ${CLUSTERNAME} || echo "Cluster ${CLUSTERNAME} could not be deleted/ does not exist"
-k3d cluster create ${CLUSTERNAME}
+k3d cluster create ${CLUSTERNAME} --registry-use k3d-oisp.localhost:12345
 
 printf "\033[1mKubernetes cluster started\033[0m\n"
 kubectl cluster-info
@@ -12,6 +12,7 @@ kubectl cluster-info
 printf "\033[1mSet routing between docker and host for K3S\033[0m\n"
 ## Needed for Kafka testing and cert-Manager
 AGENTID=$(docker ps | grep 'k3d-${CLUSTERNAME}-server-0' | awk '{print $1}')
+## TODO check if agentid is there! It sometimes skips with docker inspect takes at least 1 arg.
 AGENTIP=$(docker inspect -f '{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' ${AGENTID})
 sudo route add -net 10.42.0.0 netmask 255.255.0.0 gw ${AGENTIP}
 sudo route add -net 10.43.0.0 netmask 255.255.0.0 gw ${AGENTIP}
