33c88ef16d07bd66f3b6b7844c53b56170c31e22
Ali Rasim Kocal <arkocal@posteo.net>
2021-05-12 18:24:53 +0200

    Upgrade: Fix make upgrade and relevant util scripts.
    Signed-off-by: Ali Rasim Kocal <arkocal@posteo.net>

--- wait-until-ready.sh
+++ wait-until-ready.sh
@@ -97,6 +97,6 @@ check_deployment kairosdb ${NAMESPACE} mqtt-server 60
 check_deployment websocket-server ${NAMESPACE} mqtt-server 60
 check_job dbsetup ${NAMESPACE} 60
 check_sts keycloak ${NAMESPACE} 60
-check_beamservice rule-engine ${NAMESPACE} 120
+check_beamservice rule-engine ${NAMESPACE} 240
 printf "\ndone\n"
 exit 0;
