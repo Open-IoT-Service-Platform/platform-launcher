2722146e27b0812555a529970712d8dd6beafec8
Ali Rasim Kocal <arkocal@posteo.net>
2021-09-16 14:51:20 +0200

    CI: Fix develop branch cleanup after fails.
    Signed-off-by: Ali Rasim Kocal <arkocal@posteo.net>

--- util/restart-cluster.sh
+++ util/restart-cluster.sh
@@ -14,13 +14,13 @@ k3d cluster create ${CLUSTERNAME} --registry-use k3d-oisp.localhost:12345 --imag
 printf "\033[1mKubernetes cluster started\033[0m\n"
 kubectl cluster-info
 
-printf "\033[1mSet routing between docker and host for K3S\033[0m\n"
+# printf "\033[1mSet routing between docker and host for K3S\033[0m\n"
 ## Needed for Kafka testing and cert-Manager
-AGENTID=$(docker ps | grep "k3d-${CLUSTERNAME}-server-0" | awk '{print $1}')
+# AGENTID=$(docker ps | grep "k3d-${CLUSTERNAME}-server-0" | awk '{print $1}')
 ## TODO check if agentid is there! It sometimes skips with docker inspect takes at least 1 arg.
-AGENTIP=$(docker inspect -f '{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' ${AGENTID})
-sudo route add -net 10.42.0.0 netmask 255.255.0.0 gw ${AGENTIP}
-sudo route add -net 10.43.0.0 netmask 255.255.0.0 gw ${AGENTIP}
+# AGENTIP=$(docker inspect -f '{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' ${AGENTID})
+# sudo route add -net 10.42.0.0 netmask 255.255.0.0 gw ${AGENTIP}
+# sudo route add -net 10.43.0.0 netmask 255.255.0.0 gw ${AGENTIP}
 
 printf "\033[1mWaiting for traefik to come up\033[0m\n"
 while [ -z "$(kubectl -n kube-system get pods -l=app=traefik --ignore-not-found)" ]; do
