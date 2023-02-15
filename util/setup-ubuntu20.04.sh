4f5639096786cf331834ffe8187c30b00e5730c1
yshashix <yatindra.shashi@intel.com>
2023-02-15 19:33:31 +0100

    Added spb-e2e bat test to verify SparkplugB NGSI-LD entities at Kafka channel by sending msg from  updated oisp-iot-agent v2.4.1

--- util/setup-ubuntu20.04.sh
+++ util/setup-ubuntu20.04.sh
@@ -71,7 +71,7 @@ sudo pip install shyaml
 curl -sL https://raw.githubusercontent.com/creationix/nvm/v0.35.3/install.sh -o install_nvm.sh
 bash install_nvm.sh
 source ~/.profile
-nvm install 10
+nvm install 16
 sudo npm install -g nodemailer
 sleep 3
 
